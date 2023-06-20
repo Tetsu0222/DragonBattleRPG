3dd82ae6dbb98e0f474fa51b888305e5e44293fd
tetsu <root2120@gmail.com>
2023-06-20 19:17:57 +0900

    新設

---
+++ src/main/java/com/example/dragonbattlerpg/DragonBattleRpgApplication.java
@@ -0,0 +1,13 @@
+package com.example.dragonbattlerpg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DragonBattleRpgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DragonBattleRpgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dragonbattlerpg/DragonBattleRpgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dragonbattlerpg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DragonBattleRpgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
